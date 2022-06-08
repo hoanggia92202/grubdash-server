7ff8cb20e0f028e401953612709414e0b52731f0
duc le <ducle@ducs-MacBook-Pro.local>
2022-06-08 11:46:12 -0700

    added hasProperty middleware to POST /dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,11 +6,47 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
+// middleware handler
+function hasProperty(validPropertyName){
+    return function(req, res, next){
+        const propertyList = Object.keys(req.body.data);
+        if(propertyList.includes(validPropertyName)){
+            return next();
+        }
+        next({status: 400, message: `Dish must include a ${validPropertyName}`});
+    }
+}
+
 // TODO: Implement the /dishes handlers needed to make the tests pass
 const list = (req, res) => {
   res.json({ data: dishes });
 };
 
+const create = async (req, res) => {
+  const iD = await nextId();
+  const {
+    data: { name, description, image_url, price },
+  } = req.body;
+
+  const newDish = {
+    id: iD,
+    name,
+    description,
+    image_url,
+    price,
+  };
+
+  dishes.push(newDish);
+  res.json({ data: newDish });
+};
+
 module.exports = {
   list,
+  create: [
+    hasProperty("name"),
+    hasProperty("description"),
+    hasProperty("price"),
+    hasProperty("image_url"),
+    create,
+  ],
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const dishesController = require("./dishes.controller");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
-router.route("/").get(dishesController.list);
-router.route("/:dishID");
+router.route("/").get(dishesController.list).post(dishesController.create);
+//router.route("/:dishID");
 
 module.exports = router;
